d0c4ce34ae58303d755c57b9f7f3866bfe18df0c
Quentin Peter <qaep2@cam.ac.uk>
2019-10-15 09:41:30 +0100

    initial commit

---
+++ applaunchservices.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 14 23:47:20 2019
+
+@author: quentinpeter
+"""
+
+import os
+from LaunchServices import (LSCopyDefaultRoleHandlerForContentType,
+                            LSSetDefaultRoleHandlerForContentType,
+                            LSCopyDefaultHandlerForURLScheme,
+                            LSSetDefaultHandlerForURLScheme,
+                            kLSRolesNone,
+                            kLSRolesViewer,
+                            kLSRolesEditor,
+                            kLSRolesShell,
+                            kLSRolesAll)
+
+from AppKit import NSRunningApplication
+
+
+name = "applaunchservices"
+
+kLSRoles = {
+    'none': kLSRolesNone,
+    'viewer': kLSRolesViewer,
+    'editor': kLSRolesEditor,
+    'shell': kLSRolesShell,
+    'all': kLSRolesAll
+    }
+
+
+def get_bundle_identifier(pid=None):
+    """
+    Get bundle identifier for the given process identifier.
+
+    if pid is None, the current process pid is used.
+    """
+    if pid is None:
+        pid = os.getpid()
+    app = NSRunningApplication.runningApplicationWithProcessIdentifier_(pid)
+    if app is None:
+        return
+    return app.bundleIdentifier()
+
+
+def set_UTI_handler(uniform_type_identifier, bundle_identifier,
+                    role='all'):
+    """Set handler for given uniform type identifier and role."""
+    LSSetDefaultRoleHandlerForContentType(
+        uniform_type_identifier, kLSRoles[role], bundle_identifier)
+
+
+def get_UTI_handler(uniform_type_identifier, role='all'):
+    """Get handler for given uniform type identifier and role."""
+    return LSCopyDefaultRoleHandlerForContentType(
+        uniform_type_identifier, kLSRoles[role])
+
+
+def set_URL_scheme_handler(url_scheme, bundle_identifier):
+    """Set handler for given URL scheme and role."""
+    LSSetDefaultHandlerForURLScheme(url_scheme, bundle_identifier)
+
+
+def get_URL_scheme_handler(url_scheme):
+    """Get handler for given URL scheme and role."""
+    return LSCopyDefaultHandlerForURLScheme(url_scheme)
---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 15 09:33:49 2019
+
+@author: quentinpeter
+"""
+
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="applaunchservices",
+    version="0.1.0",
+    author="Quentin Peter",
+    author_email="qpeter@bluewin.ch",
+    description="Simple package for registering an app with apple Launch Services to handle UTI and URL",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/impact27/applaunchservices",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+    ],
+    python_requires='>=3.6',
+)
